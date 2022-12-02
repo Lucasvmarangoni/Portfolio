591e39be89cfa1b8e3d830abd8da3e9964dc035c
Ldragk <lucasvm.ti@gmail.com>
2022-12-02 19:27:58 -0300

    att

---
+++ src/components/portfolio/Port.tsx
@@ -0,0 +1,94 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { Slides } from "../functions/Slides";
+import "./styles.css";
+
+const MOver = () => {
+  const popup: any = document.querySelector("#myPopup");
+  popup.style = "visibility: visible;";
+};
+const MOut = () => {
+  const popup: any = document.querySelector("#myPopup");
+  popup.style = "visibility: hidden;";
+};
+
+export const Port = () => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger
+        asChild
+        className="m-auto 
+     "
+      >
+        <div className="divCapa">
+          <button>
+            <img id="mouse" className="projCapa border border-[#1b1b1bab]" src="/readme.png" alt="" />
+          </button>
+          <h2 className="imgTitle">Website Portfólio</h2>
+        </div>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="DialogOverlay" />
+        <Dialog.Content className="DialogContent focus:outline-none">
+          <Dialog.Title className="DialogTitle"></Dialog.Title>
+
+          <Dialog.Description className="Dgrid">
+            <h1 className="projTitle">
+              Website Portfólio - <span className="projYear">2022</span>
+            </h1>
+            <div id="img" className="banner">
+              <img src="/readme.png"></img>
+            </div>
+
+            <div className="flex gap-1">
+              <div className="flex gap-2">
+                <div className="Icon">
+                  <img className="h-5" src="/github-proj.png" alt="" />
+                  <a
+                    className="hover:underline"
+                    href="https://github.com/Ldragk/Portfolio"
+                  >
+                    Github
+                  </a>
+                </div>
+              </div>
+              <div onMouseOver={MOver} onMouseOut={MOut}>
+                <span
+                  className="invisible text-T2 bg-BGH p-2 rounded-md ml-4 absolute"
+                  id="myPopup"
+                >
+                  Existem muitos lugares para clicar!
+                </span>
+                <img
+                  className="w-4 cursor-pointer opacity-80"
+                  src="/info.png"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div className="mt-2">
+              <p className="P">
+                Projeto Web desenvolvido para minha apresentação pessoal e
+                exposição de meus trabalhos pessoais como Software Engineer.
+              </p>
+
+              <div className="divTec">
+                <h2 className="hTec">Tecnologias </h2>
+                <p className="P">
+                  TypeScript, Node NPM, Vite.js, React.js, Radix ui, TailWind
+                </p>
+              </div>
+            </div>
+          </Dialog.Description>
+
+          <Dialog.Close asChild>
+            <button className="IconButton">
+              <Cross2Icon />
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,12 +1,13 @@
 import { Esports } from "../components/portfolio/Esports";
 import { Lab01 } from "../components/portfolio/Lab01";
+import { Port } from "../components/portfolio/Port";
 
 export const Portfolio = () => {
   return (
     <div
-      className="mt-10 w-[1030px] py-20 bg-BGH m-auto flex justify-center flex-wrap 
-       n0:w-[1030px]   
-       n1:w-[830px] n1:gap-0   
+      className="mt-10 w-[1030px] py-20 bg-BGH m-auto flex flex-wrap
+       n0:w-[1030px]    
+       n1:w-[830px]
        n2:w-[781px] n2:mt-10 n2:gap-14
        n3:w-[576px] n3:mt-6 n3:gap-14  
        n4:w-[530px] n4:mt-6 n4:gap-14
@@ -17,6 +18,7 @@ export const Portfolio = () => {
     >
       <Esports />
       <Lab01 />
+      <Port />
     </div>
   );
 };
