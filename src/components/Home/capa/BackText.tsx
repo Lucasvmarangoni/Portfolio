a677dc26e31dd21b5311204369f663e7ca458d4b
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2022-12-26 13:28:43 -0300

    att:  BackText

--- src/components/Home/capa/BackText.tsx
+++ src/components/Home/capa/BackText.tsx
@@ -2,12 +2,11 @@ export function BackText() {
   return (
     <div>
       <p className="">
-        Meus estudos estão focados em desenvolvimento Back-end com Node.js, de
-        forma subsidiária estudo Engenharia de Software e segurança de
-        aplicações web.
+        Meus estudos estão focados em Back-end com Node.js, de forma subsidiária
+        estudo Engenharia de Software e segurança de aplicações web.
       </p>
       <p>
-        A graduação em ADS inicia em 2023, antes disso já tenho estudado a
+        A graduação em ADS inicia em 2023, antes disso já estou estudando a
         maior parte do conteúdo que será abordado e pretendo iniciar um Pós
         graduação em Cibersegurança e Governança de Dados durante a graduação de
         ADS.
