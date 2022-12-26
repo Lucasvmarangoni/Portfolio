2706aa789605caea3ed6d3ad4c66abdbe6fe56c2
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2022-12-26 05:54:53 -0300

    att: BackText

--- src/components/Home/capa/BackText.tsx
+++ src/components/Home/capa/BackText.tsx
@@ -3,9 +3,9 @@ export function BackText() {
     <div>
       <p className="">
         A escolha de ser Desenvolvedor Back-end foi pelas características que
-        essa área possui, que correspondem mais as minhas características
+        essa área possui, que correspondem mais às minhas características
         pessoais. A CyberSecurity sempre me interessou, principalmente pela
-        seriedade que envolve, o que causa uma sensaçao de adrenalina, e por
+        seriedade que envolve, o que causa uma sensação de adrenalina, e por
         isso pretendo me especializar.
       </p>
       <p>
