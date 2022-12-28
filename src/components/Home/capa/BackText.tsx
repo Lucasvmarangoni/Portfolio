34b614c4355678aa59b8c483e69d276cc6125886
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2022-12-28 01:33:05 -0300

    att:  BackText

--- src/components/Home/capa/BackText.tsx
+++ src/components/Home/capa/BackText.tsx
@@ -2,9 +2,11 @@ export function BackText() {
   return (
     <div>
       <p className="">
-        Atualmente meus estudos estão focados em Desenvolvimento Back-end com
-        Node.js, desenvolvendo alguns projetos. Além disso, estou estudando também
-        Engenharia de Software e Segurança das aplicações e sistemas.
+        A graduação em ADS inicia em 2023, antes disso já estou estudando a
+        maior parte do conteúdo que será abordado. Atualmente meus estudos estão
+        focados em Desenvolvimento Back-end com Node.js, desenvolvendo alguns
+        projetos. Além disso, estou estudando também Engenharia de Software e
+        Segurança das aplicações e sistemas.
       </p>
     </div>
   );
