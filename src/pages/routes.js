b783498193c826bde66c39174014ad3ba3c89abc
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-01-15 18:52:22 -0300

    att: testando rotas

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -5,14 +5,17 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
+    req.url = '/';
     res.sendFile(path.join(__dirname, 'build', 'Home.tsx'));
 });
 
 app.get('/portfolio', function (req, res) {
+    req.url = '/portfolio';
     res.sendFile(path.join(__dirname, 'build', 'Portfolio.tsx'));
 });
 
 app.get('*', function (req, res) {
+    req.url = '*';
     res.sendFile(path.join(__dirname, 'build', 'PageNotFound.tsx'));
 });
 
