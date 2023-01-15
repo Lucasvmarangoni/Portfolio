c46808a26dab0d683ca048c7afe8be1d7e994aa7
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-01-15 19:39:43 -0300

    att: voltando para HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,20 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {
   BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-  Routes,
+  HashRouter,  
 } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
-import { Header } from "./components/Home/Header";
-import { motion } from "framer-motion";
-import { ErrorNotFound } from "./pages/ErrorNotFound";
-import { Home } from "./pages/Home";
-import { Portfolio } from "./pages/Portfolio";
+
+
 
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <HashRouter> */}
-    <BrowserRouter>
+    <HashRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
-    {/* </HashRouter> */}
+    {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
 function createBrowserHistory() {
