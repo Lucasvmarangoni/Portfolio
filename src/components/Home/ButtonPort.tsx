7c7d2a2d689ebe8295460af8a818e2b6079ee6bd
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-01-22 03:03:07 -0300

    ⚒  Alt: menu

--- src/components/Home/ButtonPort.tsx
+++ src/components/Home/ButtonPort.tsx
@@ -13,17 +13,16 @@ export const ButtonPort = () => {
     "
     >
       <button
-        className="w-48 h-16  
+        className="w-48 h-16 
         text-3xl font-medium text-T1
         border-[#55396285] border-[2px] 
         opacity-50 hover:opacity-70 brightness-150
-        setinha hover:after:right-7
-        n3:w-48 n3:h-16 n3:text-2xl
-        n4:w-32 n4:h-14 n4:text-2xl
-        n5:w-32 n5:h-14 n5:text-2xl
-        n6:w-32 n6:h-14 n6:text-2xl
-        n7:w-32 n7:h-14 n7:text-2xl
-        
+        setinha hover:after:right-7          
+        n3:w-48 n3:h-16 n3:text-3xl n3:hover:after:right-7
+        n4:w-36 n4:h-14 n4:text-2xl n4:hover:after:right-4
+        n5:w-36 n5:h-14 n5:text-2xl n5:hover:after:right-4
+        n6:w-36 n6:h-14 n6:text-2xl n6:hover:after:right-4
+        n7:w-36 n7:h-14 n7:text-2xl n7:hover:after:right-4        
         "
       >
         <Link to="/portfolio">
