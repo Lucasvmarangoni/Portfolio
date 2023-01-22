fbe79854b1b540601452a554723538abe5a22ce9
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-01-22 02:31:24 -0300

    ⚒  Add: responsividade a ButtonPort

--- src/components/Home/ButtonPort.tsx
+++ src/components/Home/ButtonPort.tsx
@@ -17,7 +17,14 @@ export const ButtonPort = () => {
         text-3xl font-medium text-T1
         border-[#55396285] border-[2px] 
         opacity-50 hover:opacity-70 brightness-150
-        setinha hover:after:right-7"
+        setinha hover:after:right-7
+        n3:w-48 n3:h-16 n3:text-2xl
+        n4:w-32 n4:h-14 n4:text-2xl
+        n5:w-32 n5:h-14 n5:text-2xl
+        n6:w-32 n6:h-14 n6:text-2xl
+        n7:w-32 n7:h-14 n7:text-2xl
+        
+        "
       >
         <Link to="/portfolio">
           <h1 className="">Portfólio</h1>
