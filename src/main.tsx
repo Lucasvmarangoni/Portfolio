251e8d2ee6ba32d512d6be55fe9a0140ceda80ef
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-02-18 02:45:05 -0300

    ⚒

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import * as ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
 if (window.location.hash) {
-  window.history.replaceState("", "https://lucasvmarangoni.vercel.app/", "portfolio");
+  window.history.replaceState("", "https://lucasvmarangoni.vercel.app/", window.location.hash.substr(1));
+  // window.history.replaceState("", "https://lucasvmarangoni.vercel.app/", "portfolio");
   console.log(document.title)
   console.log(window.location.pathname);
   ;
