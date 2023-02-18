98c1565a36f6782deb822bb55ca7080278846511
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-02-18 01:43:40 -0300

    ⚒

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <HashRouter> */}
-      <BrowserRouter>
+    <HashRouter>
+      {/* <BrowserRouter> */}
       <App />
-      </BrowserRouter>
-    {/* </HashRouter> */}
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
 function createBrowserHistory() {
