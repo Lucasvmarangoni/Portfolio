c1d6b90a18b9df718686ef73413047bfba9346a6
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-02-18 02:47:42 -0300

    ⚒

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
-if (window.location.hash) {
-  window.history.replaceState("", "https://lucasvmarangoni.vercel.app/", window.location.hash.substr(1));
-  // window.history.replaceState("", "https://lucasvmarangoni.vercel.app/", "portfolio");
-  console.log(document.title)
-  console.log(window.location.pathname);
-  ;
-  
-}
+// if (window.location.hash) {
+//   window.history.replaceState("", "https://lucasvmarangoni.vercel.app/", window.location.hash.substr(1));
+//   // window.history.replaceState("", "https://lucasvmarangoni.vercel.app/", "portfolio");
+//   console.log(document.title)
+//   console.log(window.location.pathname);
+//   ;
+
+// }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
-      {/* <BrowserRouter basename={"/"}> */}
+    {/* <HashRouter> */}
+    <BrowserRouter basename={"/"}>
       <App />
-      {/* </BrowserRouter> */}
-    </HashRouter>
+    </BrowserRouter>
+    {/* </HashRouter> */}
   </React.StrictMode>
 );
