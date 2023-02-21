c28fb06f18de1ba61008548dd565bbb648cd0d69
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-02-20 22:59:40 -0300

    ⚒

--- src/components/Home/capa/TextCapa.tsx
+++ src/components/Home/capa/TextCapa.tsx
@@ -34,7 +34,7 @@ export function TextCapa() {
       </div>
       <p className="mb-1 ">
         Sou desenvolvedor <span className="text-T1">back-end</span>, e estou
-        estudando para aprimorar minhas habilidades e conhecimentos para ser um
+        estudando para aprimorar minhas habilidades e conhecimentos como 
         <span className="text-T1"> Engenheiro de Software</span>. Além disso,
         também estou cursando o primeiro período da graduação de analise e
         desenvolvimento de sistemas na Pontifícia Universidade Católica de
