5b79a1b4bacd206478c18746fa8b55dfa8122915
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-03-03 13:58:11 -0300

    ⚒

--- src/components/portfolio/TaskList.tsx
+++ src/components/portfolio/TaskList.tsx
@@ -53,7 +53,7 @@ export const TaskList = () => {
               <div className="divTec">
                 <h2 className="hTec">Tecnologias </h2>
                 <p className="P TnotServ">
-                  TypeScript | Node.js | Express | Vitest | Prisma Studio |
+                  TypeScript | Node.js | Express | Vitest | Prisma Studio | Class-validator |
                   SQLite |
                 </p>
               </div>
