0dd4edb6b8d7b16890913ffe17192b30f0e6204e
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-04-26 04:26:33 -0300

    ⚒

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -10,9 +10,13 @@ import { TaskList } from "../components/portfolio/TaskList";
 import { SetStateAction, useEffect, useState } from "react";
 import { BestSurfing } from "../components/portfolio/BestSurfing";
 
-export const projetos: any = [<BestSurfing />, <TaskList />, <NotificationsService />, <Port />, <Esports />];
+export const autorais: any = [<TaskList />, <Port />];
+export const projetos: any = [<BestSurfing />, <NotificationsService />, <Esports />];
 
-const projetosId: string[] = ["TbestSurfing", "TtaskList", "TnotServ", "Tport", "Tesports"]
+
+const autoraisId: string[] = ["TtaskList","Tport"]
+const projetosId: string[] = ["TbestSurfing", "TnotServ", "Tesports"]
+
 
 export const Portfolio = () => {
   let [windowWidth, windowCheck]: SetStateAction<any> = useState();
@@ -104,8 +108,33 @@ export const Portfolio = () => {
         </datalist>
       </div>
 
-      <div
-        className="m-auto flex flex-wrap justify-center 
+      <div>
+
+
+        <div>
+          <motion.div
+            className=" text-center w-[70%] m-auto"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.4,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          >
+
+            <h2 className="text-T4 text-xl mt-10">Projetos pessoais</h2>
+            <p className="text-base text-[#252525] text-justify mt-5" >
+            Projetos pessoais são aqueles de minha autoria, ou seja, que crio e desenvolvo do zero sozinho. 
+            O objetivo desses projetos é praticar para consolidar novos conhecimentos e habilidades adquiridas.
+            </p>
+
+
+          </motion.div>
+        </div>
+
+        <div
+          className="m-auto flex flex-wrap justify-center 
          n2:gap-14 n2:mt-10 
          n3:gap-14 n3:mt-6   
          n4:gap-14 n4:mt-6 
@@ -113,42 +142,125 @@ export const Portfolio = () => {
          n6:gap-14 n6:mt-4 
          n7:gap-10 n7:mt-4        
       "
-      >{
-            projetos.map((projeto: any, transitionDelay: number ) => {
-              return   (
-               
-              <motion.div
-              id={projetosId[transitionDelay]}
+        >
+
+          {
+            autorais.map((projeto: any, transitionDelay: number) => {
+              return (
+
+                <motion.div
+                  id={autoraisId[transitionDelay]}
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    duration: 0.8,
+                    delay: transitionDelay / 4,
+                    ease: [0, 0.71, 0.2, 1.01],
+                  }}
+                >
+                  {projeto}
+                </motion.div>
+              )
+            })
+
+          }
+          {           
+          (autorais.length % 2) == 0 ?
+            null
+            :
+            <motion.div
+              id="THidden"
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
                 duration: 0.8,
-                delay: transitionDelay / 4,
+                delay: 1.0,
                 ease: [0, 0.71, 0.2, 1.01],
               }}
-            >               
-      
-                {projeto}
-           
-             
+            >
+              : <Hidden />
             </motion.div>
-            )})        
-            
-          }     
-          
-        <motion.div
-          id="THidden"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.8,
-            delay: 1.0,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
+          }
+        </div>
+
+          <hr  className=" border-[#252525] w-[80%] m-auto
+           n2:mt-10 
+           n3:mt-6   
+           n4:mt-6 
+           n5:mt-6 
+           n6:mt-4 
+           n7:mt-4        
+          "/>
+
+        <div
+          className="m-auto flex flex-wrap justify-center 
+         n2:gap-14 n2:mt-10 
+         n3:gap-14 n3:mt-6   
+         n4:gap-14 n4:mt-6 
+         n5:gap-14 n5:mt-6 
+         n6:gap-14 n6:mt-4 
+         n7:gap-10 n7:mt-4        
+      "
         >
-          { projetos.length % 2 == 0 ? null : <Hidden /> }
-        </motion.div>
-        
+          <motion.div
+            className=" text-center w-[70%] m-auto"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.4,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          >
+
+            <h2 className="text-T4 text-xl">Projetos de cursos</h2>
+            <p className="text-base text-[#252525] text-justify mt-5" >
+            Projetos de curso são aqueles desenvolvidos durante os cursos que faço, ou seja, sua criação não é de minha autoria. 
+            O objetivo desses projetos é aprender para obter novos conhecimentos e habilidades.
+            </p>
+
+
+          </motion.div>
+
+          {
+            projetos.map((projeto: any, transitionDelay: number) => {
+              return (
+
+                <motion.div
+                  id={projetosId[transitionDelay]}
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    duration: 0.8,
+                    delay: transitionDelay / 4,
+                    ease: [0, 0.71, 0.2, 1.01],
+                  }}
+                >
+                  {projeto}
+                </motion.div>
+              )
+            })
+
+          }
+          {
+            projetos.length % 2 == 0 ?
+              null
+              :
+              <motion.div
+                id="THidden"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.8,
+                  delay: 1.0,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+              >
+                : <Hidden />
+              </motion.div>
+          }
+        </div>
+
       </div>
     </motion.div>
   );
