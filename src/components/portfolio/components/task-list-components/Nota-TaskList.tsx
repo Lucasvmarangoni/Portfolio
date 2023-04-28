09ee8b559f7b5d946c04f0fa918b9018071c1ae5
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-04-27 22:00:09 -0300

    ⚒

--- src/components/portfolio/components/task-list-components/Nota-TaskList.tsx
+++ src/components/portfolio/components/task-list-components/Nota-TaskList.tsx
@@ -40,7 +40,7 @@ export const NotaTaskList = () => {
           <p className="P mt-5">
           <img src="https://img.shields.io/badge/version-3.0.0-white" alt="Versão 3.0.0"></img>
               A versão 3.0.0 já esta em planejamento. Será aplicado o conhecimento do projeto 
-              <a href="https://github.com/Lucasvmarangoni/Best-Surfing" target="_blank"> Best-Surfing</a> 
+              <a href="https://github.com/Lucasvmarangoni/Best-Surfing" target="_blank"> Best-Surfing </a> 
               afim de aprimorar o projeto e consolidar esses novos conhecimentos.
           </p>
         </Collapsible.Content>
