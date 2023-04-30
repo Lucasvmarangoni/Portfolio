abff01debaf1e3fac0ff8b80aad20f38ab1cea82
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-04-30 18:34:30 -0300

    ⚒

--- src/components/Home/Whatsapp.tsx
+++ src/components/Home/Whatsapp.tsx
@@ -16,69 +16,62 @@ export function Whatsapp() {
 
     <Dialog.Description className="mt-0">
       <div className="grid gap-4 justify-center">
-        <form
-          id="form"
-          method="POST"          
-          action={link}
-          target="_blank"
-          className="grid row-span-3 gap-3"
-        >
 
 
-          <div className="text-T2 grid gap-2 w-5 ">
-            <label htmlFor="inputName">Nome:</label>
-            <input
-              className="bg-BGH border-[#ffffff0e] border-[0.5px]"
-              id="inputName"
-              name="name"
-              type="text"
-              required
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              value={name}
-            />
-          </div>
+        <div className="text-T2 grid gap-2 w-5 ">
+          <label htmlFor="inputName">Nome:</label>
+          <input
+            className="bg-BGH border-[#ffffff0e] border-[0.5px]"
+            id="inputName"
+            name="name"
+            type="text"
+            required
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+          />
+        </div>
 
-          <div className="text-T2 grid gap-2 w-5 ">
-            <label htmlFor="inputName">Empresa:</label>
-            <input
-              className="bg-BGH border-[#ffffff0e] border-[0.5px]"
-              id="inputName"
-              name="name"
-              type="text"
-              required
-              onChange={(e) => {
-                setCompany(e.target.value);
-              }}
-              value={company}
-            />
-          </div>
+        <div className="text-T2 grid gap-2 w-5 ">
+          <label htmlFor="inputName">Empresa:</label>
+          <input
+            className="bg-BGH border-[#ffffff0e] border-[0.5px]"
+            id="inputName"
+            name="name"
+            type="text"
+            required
+            onChange={(e) => {
+              setCompany(e.target.value);
+            }}
+            value={company}
+          />
+        </div>
 
 
-          <div className="text-T2 grid gap-2 w-64">
-            <label className="text-T2" htmlFor="inputMessage">
-              Mensagem:
-            </label>
-            <textarea
-              className="bg-BGH border-[#ffffff0e] border-[0.5px]"
-              id="inputMessage"
-              name="message"
-              required
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-            />
-          </div>
-        </form>
+        <div className="text-T2 grid gap-2 w-64">
+          <label className="text-T2" htmlFor="inputMessage">
+            Mensagem:
+          </label>
+          <textarea
+            className="bg-BGH border-[#ffffff0e] border-[0.5px]"
+            id="inputMessage"
+            name="message"
+            required
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+        </div>
+
         <button
-          form="form"
           type="submit"
           className="bg-BGB w-64 font-semibold py-2 text-base setinha hover:after:right-[103px] hover:opacity-70"
           disabled={!message}
+          
         >
-          Enviar
+          <a href={link} target="_blank">Enviar</a>
         </button>
       </div>
     </Dialog.Description>
