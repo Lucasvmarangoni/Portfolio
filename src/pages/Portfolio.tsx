8374a9dba2ce093fbff284278c95862bcacc039f
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-11 18:08:29 -0300

    ✬

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -262,12 +262,7 @@ export const Portfolio = () => {
 
         <div
           className="m-auto flex flex-wrap justify-center 
-          gap-10 n2:mt-10 
-          n3:mt-6   
-          n4:mt-6 
-          n5:mt-6 
-          n6:mt-4 
-          n7:mt-4        
+          gap-10     
       "
         >
           {
