0f72ca1e0a4c9ad139b164f6fead7514b6bb348d
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-21 17:51:35 -0300

    ✬

--- src/components/portfolio/components/task-list-components/Nota-TaskList.tsx
+++ src/components/portfolio/components/task-list-components/Nota-TaskList.tsx
@@ -19,16 +19,17 @@ export const NotaTaskList = () => {
         </Collapsible.Trigger>
 
         <Collapsible.Content className="CollapsibleContent">
-          <p className="P mt-2">            
+          <p className="P mt-2">
             <img src="https://img.shields.io/badge/version-1.0.0-white" alt="Versão 1.0.0"></img>
             O projeto teve seu desenvolvimento iniciado de acordo com o aprendizado do evento
             <a href="https://github.com/Lucasvmarangoni/NLW-Ignite-2022" target="_blank">NLW ESpots Ignite</a>
             da RocketSeat, ou seja, um back-end simples e básico.
             Após alcançar esse objetivo, ainda pretendia fazer outras coisas (funcionalidades) antes de começar a aprimorar a engenharia e arquitetura do software, mas comecei a aprimorar algumas coisas e acabei decidindo por já organizar tudo logo.
-          </p>       
+          </p>
 
           <p className="P mt-5">
-          <img src="https://img.shields.io/badge/version-2.0.0-white" alt="Versão 2.0.0"></img>
+            <a href="https://github.com/Lucasvmarangoni/TaskList/commit/92bf7f0" title="clique para ver o código da versão 2.0.0">
+              <img src="https://img.shields.io/badge/version-2.0.0-white" alt="Versão 2.0.0"></img></a>
             Então comecei a aplicar os conhecimento obtidos com o evento
             <a href="https://github.com/Lucasvmarangoni/notifications-service" target="_blank">Ignite Lab 04 - Notification Service</a>
             da RocketSeat. Ao longo dessa etapa, mudei muitas decisões iniciais. Por não ter iniciado com TDD, alguns testes unitários
@@ -38,10 +39,16 @@ export const NotaTaskList = () => {
           </p>
 
           <p className="P mt-5">
-          <img src="https://img.shields.io/badge/version-3.0.0-white" alt="Versão 3.0.0"></img>
-              A versão 3.0.0 já esta em planejamento. Será aplicado o conhecimento adquirido no projeto 
-              <a href="https://github.com/Lucasvmarangoni/Best-Surf" target="_blank"> Best-Surf </a> 
-              além de outros.
+            <a href="" title="clique para ver o código da versão 2.1.0"><img src="https://img.shields.io/badge/version-2.1.0-black" alt="Versão 2.1.0"></img></a>
+            O projeto será aprimorado com novos conhecimentos que obtive com o projeto <a href="https://github.com/Lucasvmarangoni/Best-Surf" target="_blank">Best-Surf</a>.
+            Nesta atualização, vou realizar a alteração de nomenclaturas, aprimorar testes unitários, Graceful shutdown, tratamento de erros, status code, logging e cache.
+          </p>
+
+          <p className="P mt-5">
+            <img src="https://img.shields.io/badge/version-3.0.0-white" alt="Versão 3.0.0"></img>
+            A versão 3.0.0 já esta em planejamento. Será aplicado o conhecimento adquirido no projeto
+            <a href="https://github.com/Lucasvmarangoni/Best-Surf" target="_blank"> Best-Surf </a>
+            além de outros.
           </p>
         </Collapsible.Content>
       </div>
