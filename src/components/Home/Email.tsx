75db85b6bbe96f8311bfcda6c8d363f4eff75584
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-06-07 00:57:54 -0300

    ✬

--- src/components/Home/Email.tsx
+++ src/components/Home/Email.tsx
@@ -1,107 +1,124 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { useState } from "react";
 import { MOver, MOut, SpanTag, SendObligatoryButton } from "./capa/functions/Obligatory-itens";
+import axios from "axios";
 
 export function Email() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [description, setDescription] = useState("");
 
+    function handleSubmit(event: any) {
+        event.preventDefault()
+        axios.get("https://lucasvmarangoni.vercel.app/send-mail")
+            .then(response => {
+                if (response.status === 200) {
+                    return response.data;
+                } else {
+                    throw new Error("Erro na requisição");
+                }
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
     return (
-        <Dialog.Description className="mt-0">
-            <div className="grid gap-4 justify-center">
-                <form
-                    id="form"
-                    method="POST"
-                    action="https://formsubmit.co/lucasvm.ti@gmail.com"
-                    target="_blank"
-                    className="grid row-span-3 gap-3"
-                >
-                    <div className="text-T2 grid gap-2 w-5 ">
-                        <label htmlFor="inputName">Nome:</label>
-                        <input
-                            className="bg-BGH border-[#ffffff0e] border-[0.5px]"
-                            id="inputName"
-                            name="name"
-                            type="text"
-                            required
-                            onChange={(e) => {
-                                setName(e.target.value);
-                            }}
-                            value={name}
-                        />
-                    </div>
+        <Dialog.Description className="mt-0 grid gap-4 justify-center">
+            <form
+                id="form"
+                // method="POST"
+                // action="https://formsubmit.co/lucasvm.ti@gmail.com"
+                target="_blank"
+                onSubmit={handleSubmit}
+                className="grid row-span-3 gap-3"
+            >
+                <div className="text-T2 grid gap-2 w-5 ">
+                    <label htmlFor="inputName">Nome:</label>
+                    <input
+                        className="bg-BGH border-[#ffffff0e] border-[0.5px]"
+                        id="inputName"
+                        name="name"
+                        type="text"
+                        required
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                        value={name}
+                    />
+                </div>
 
-                    <div className="text-T2 grid gap-2 w-5 ">
-                        <label className="" htmlFor="inputEmail">
-                            Email:
-                        </label>
-                        <input
-                            className="bg-BGH border-[#ffffff0e] border-[0.5px]"
-                            id="inputEmail"
-                            name="email"
-                            type="email"
-                            required
-                            onChange={(e) => {
-                                setEmail(e.target.value);
-                            }}
-                            value={email}
-                        />
-                    </div>
+                <div className="text-T2 grid gap-2 w-5 ">
+                    <label className="" htmlFor="inputEmail">
+                        Email:
+                    </label>
+                    <input
+                        className="bg-BGH border-[#ffffff0e] border-[0.5px]"
+                        id="inputEmail"
+                        name="email"
+                        type="email"
+                        required
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        value={email}
+                    />
+                </div>
 
-                    <div className="text-T2 grid gap-2 w-64">
-                        <label className="text-T2" htmlFor="inputDescription">
-                            Mensagem:
-                        </label>
-                        <textarea
-                            className="bg-BGH border-[#ffffff0e] border-[0.5px]"
-                            id="inputDescription"
-                            name="description"
-                            required
-                            onChange={(e) => {
-                                setDescription(e.target.value);
-                            }}
-                            value={description}
-                        />
-                    </div>
-                </form>
-                <button
-                    onMouseOver={() => {
-                        MOver(email && description);
-                    }}
-                    onMouseOut={() => {
-                        MOut(email && description);
-                    }}
-                    form="form"
-                    type="submit"
-                    className="w-64 brightness-150 grid justify-center items-center"
-                    disabled={!email || !description}
+                <div className="text-T2 grid gap-2 w-64">
+                    <label className="text-T2" htmlFor="inputDescription">
+                        Mensagem:
+                    </label>
+                    <textarea
+                        className="bg-BGH border-[#ffffff0e] border-[0.5px]"
+                        id="inputDescription"
+                        name="description"
+                        required
+                        onChange={(e) => {
+                            setDescription(e.target.value);
+                        }}
+                        value={description}
+                    />
+                </div>
+            </form>
+            <button
+                onMouseOver={() => {
+                    MOver(email && description);
+                }}
+                onMouseOut={() => {
+                    MOut(email && description);
+                }}
+                form="form"
+                type="submit"
+                className="w-64 brightness-150 grid justify-center items-center"
+                disabled={!email || !description}
 
-                    onClick={(event) => {
-                        alert("Email enviada com sucesso!")
-                        const inputName = document.querySelector("#inputName") as HTMLInputElement
-                        const inputEmail = document.querySelector("#inputEmail") as HTMLInputElement
-                        const inputDescription = document.querySelector("#inputDescription") as HTMLInputElement
-                        if (inputName) {
-                            inputName.value = "";
-                        }
-
-                        if (inputEmail) {
-                            inputEmail.value = "";                                                
-                        }
-
-                        if (inputDescription) {
-                            inputDescription.value = "";
-                        }
-                        event.preventDefault()
-
+                onClick={() => {
+                    alert("Email enviada com sucesso!")
+                    const inputName = document.querySelector("#inputName") as HTMLInputElement
+                    const inputEmail = document.querySelector("#inputEmail") as HTMLInputElement
+                    const inputDescription = document.querySelector("#inputDescription") as HTMLInputElement
+                    if (inputName) {
+                        inputName.value = "";
                     }
+
+                    if (inputEmail) {
+                        inputEmail.value = "";
                     }
-                >
-                    <span className={`${SendObligatoryButton}`}>Enviar</span>
-                    {SpanTag("Email e mensagem obrigatórios!")}
-                </button>
-            </div>
+
+                    if (inputDescription) {
+                        inputDescription.value = "";
+                    }
+
+                }
+                }
+            >
+                <span className={`${SendObligatoryButton}`}>Enviar</span>
+                {SpanTag("Email e mensagem obrigatórios!")}
+            </button>
         </Dialog.Description>
 
 
---
+++ src/components/Home/back-end/send-mail.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('https://lucasvmarangoni.vercel.app/send-mail', (req, res) => {
+    const { name, email, message } = req.body;
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'lucasvmarangoni.sendemail@gmail.com',
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+   
+    const mailOptions = {
+        from: 'lucasvmarangoni.sendemail@gmail.com',
+        to: 'lucasvm.ti@gmail.com',
+        subject: '⭐ - CONTATO DO PORTFÓLIO - ⭐',
+        text: `Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {            
+            res.status(500).json({ error: 'Erro ao enviar o email' });
+        } else {          
+            res.json({ success: true });
+        }
+    });
+});
+
+
