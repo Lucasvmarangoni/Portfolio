148fb66eff1bb4b9391a070720b7e5ca176be66d
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-08-27 11:42:02 -0300

    ✬

--- src/components/Home/info/First.tsx
+++ src/components/Home/info/First.tsx
@@ -37,13 +37,13 @@ export function First() {
         Motion(1.0, 1.8,
             <div className=" 
             flex align-center justify-center flex-wrap m-auto py-8
-            n0:gap-8 n0:mb-0
-            n7:gap-6 n7:mb-0
+            n2:gap-0 
+            n7:gap-6 
             
             ">
                 <div className="">
                     <h2 className="text-T1 brightness-200 font-semibold relative top-14 text-xl 
-                n2:text-center n2:w-[600px]
+                n2:text-center 
                 n4:top-14
                 n5:top-10
                 n6:top-8
@@ -59,9 +59,10 @@ export function First() {
                     </div>
 
 
-                <div className="m-auto flex  gap-12
-                n1:flex-col 
-                n7:flex-row
+                <div className="m-auto flex  gap-12 align-center justify-center
+                n2:flex-col 
+                n6:flex-row
+                n7:flex-col
                 ">
                     <ButtonPort />
                     <Algorithm  name="HackerRank"/>
