691ac62989a3c93baee70bfb74b92c642cc80729
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-15 15:19:26 -0300

    ✬

--- src/components/Home/info/components/images-carrosel.tsx
+++ src/components/Home/info/components/images-carrosel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import { MdClose } from "react-icons/md";
 
@@ -48,16 +48,34 @@ export const ProjectImagesCarousel: React.FC<{ images: ImagesProps[] }> = ({ ima
         }
     };
 
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (isModalOpen) {
+                if (event.key === 'ArrowRight') {
+                    goToNextImage();
+                } else if (event.key === 'ArrowLeft') {
+                    goToPreviousImage();
+                }
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [isModalOpen, goToNextImage, goToPreviousImage]);
+
     return (
-        <div className="relative m-auto max-w-3xl pr-8">
-            <div ref={carouselRef} className="w-[85%] m-auto flex items-center justify-center max-h-fit overflow-x-hidden scrollbar-hide">
+        <div className="relative m-auto max-w-3xl pr-0">
+            <div ref={carouselRef} className="w-[85%] m-auto pl-44 flex items-center justify-center max-h-fit overflow-x-hidden scrollbar-hide">
                 {images.map((image: ImagesProps, index: number) => (
                     <div key={index} className="flex-none m-1 w-1/6 transition-transform transform scale-100 hover:scale-105 cursor-pointer"
                         onClick={() => openModal(index)}>
                         <img
                             src={image.src}
                             alt={`${image.description}`}
-                            className="w-full max-h-10 object-cover rounded-lg"
+                            className="w-full h-10 object-cover rounded-lg"
                         />
                     </div>
                 ))}
@@ -66,7 +84,7 @@ export const ProjectImagesCarousel: React.FC<{ images: ImagesProps[] }> = ({ ima
         p-2 text-4xl text-white hover:text-gray-300 active:text-3xl">
                 <SlArrowLeft color="#3b27535b" />
             </button>
-            <button onClick={goToNextSlide} className="absolute top-1/2 transform -translate-y-1/2 right-7 p-2 brightness-200
+            <button onClick={goToNextSlide} className="absolute top-1/2 transform -translate-y-1/2 right-0 p-2 brightness-200
         text-4xl text-white hover:text-gray-300 active:text-3xl">
                 <SlArrowRight color="#3b27535b" />
             </button>
@@ -78,7 +96,7 @@ export const ProjectImagesCarousel: React.FC<{ images: ImagesProps[] }> = ({ ima
                             <h1 className="m-auto text-T1 brightness-150 n4:text-2xl n7:text-xl">{images[selectedImageIndex].title}</h1>
                             <p className="PP brightness-75">{images[selectedImageIndex].description}</p>
                         </div>
-                        <img src={images[selectedImageIndex].src} alt="Selected" className="w-full h-auto" />
+                        <img src={images[selectedImageIndex].src} alt="Selected" className="w- m-auto h-auto" />
 
                         <button onClick={goToPreviousImage} className="text-T2 absolute top-1/2 left-5 text-2xl">
                             <SlArrowLeft color="#ffffff85" />
