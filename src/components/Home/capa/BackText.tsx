82348ec7e96b241d2d63978392c7c1f8e5a45cd1
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-27 16:32:29 -0300

    ✬

--- src/components/Home/capa/BackText.tsx
+++ src/components/Home/capa/BackText.tsx
@@ -9,8 +9,8 @@ export function BackText() {
         Motion(0.7, 0.2,
           <p className="text-justify flex ">
             Por ter encontrado essa seção, vou contar um pouco mais sobre mim. Sou Bacharel em direito. 
-            Meu principal hobby é anime, o segundo games, principalmente mmo rpg.
-            Também gosto de musculação e calistenia e faço coleção de conchas que encontro.
+            Meu principal hobby é anime, o segundo games, principalmente MMO RPG.
+            Também gosto de musculação e calistenia, e faço coleção de conchas que encontro.
             Busco sempre estar rodeado de pessoas que fazem eu sentir que não sei de nada,
             assim mantenho a motivação para evoluir constantemente.
           </p>)
