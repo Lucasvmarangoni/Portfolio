346422568c2cd534aa4d3d5160bfe12ff4f92a1e
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-10-06 10:22:51 -0300

    ✬

--- src/components/Home/info/components/Now.tsx
+++ src/components/Home/info/components/Now.tsx
@@ -62,12 +62,12 @@ export function Now() {
           )}
 
           <div className="grid gap-4">
-            {
+            {/* {
               now(1.0, 0.7, "Curso:", "!", "Sistemas operacionais (UNIVESP - Youtube)")
             }          
             {
               now(1.0, 0.9, "Livro:", "!", "Sistemas Operacionais Modernos (Tanenbaum)")
-            }          
+            }           */}
             {
               now(1.0, 1.1, "Livro:", "!", "Entendendo algoritmos (Aditya Y. Bhargava)")
             }   
