54a6c47e1036e3a9eda963e7e36f636cd11eaf00
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2024-03-13 17:02:52 -0300

    ✬

--- src/components/Home/info/components/Certificate-carousel.tsx
+++ src/components/Home/info/components/Certificate-carousel.tsx
@@ -39,7 +39,7 @@ const images = [
     href: 'https://www.udemy.com/certificate/UC-923351e1-3128-443b-9d39-b9b8da0337e5/',
     title: `<strong>Udemy - Estrutura de Dados e Algoritmos em Python</strong>`,
     text: `Big-O, vetores, busca binária, pilha, filas, deques, listas encadeadas, recursão, 
-    algoritmos de ordenação, arvore binária e grafos. Também lí: Aditya Y. - Entendendo algorítimos`,
+    algoritmos de ordenação, arvore binária e grafos.`,
   },
   {
     src: '',
